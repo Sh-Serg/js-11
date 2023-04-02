c030c4e8928deb5449de519fc4cd29c017d2bde2
Sh-Serg <shovkun.sa@gmail.com>
2023-04-02 22:15:59 +0300

    add

--- src/js/simple-light-box.js
+++ src/js/simple-light-box.js
@@ -1,4 +1,5 @@
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 function createGallerySimplelightbox() {
   let gallery = new SimpleLightbox('.gallery a');
